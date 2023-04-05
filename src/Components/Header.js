82ba0a9869e63503c9883f0832d1ad321648a3b9
nGhart <107068059+nGhart@users.noreply.github.com>
2023-04-05 21:42:53 +0000

    Update Header.js

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -3,15 +3,15 @@ import React from 'react'
 function Header() {
   return (
     <nav>
-    <button className="brand" href='#'>PIZZAMANIA</button>
-    <div className="navLeft">
-      <ul>
-        <li>Home</li>
-        <li>About Pizza</li>
-        <li>Pizza Types</li>
-      </ul>
-    </div>
-  </nav>
+      <button className="brand" href='#'>PIZZAMANIA</button>
+      <div className="navLeft">
+        <ul>
+          <li>Home</li>
+          <li>About Pizza</li>
+           <li>Pizza Types</li>
+        </ul>
+      </div>
+    </nav>
   )
 }
 
