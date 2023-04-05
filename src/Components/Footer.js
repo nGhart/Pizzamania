9c7ffb5e10186af456a9f015002d9222db479f41
nGhart <107068059+nGhart@users.noreply.github.com>
2023-04-05 21:35:57 +0000

    Fixed indentation

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -3,8 +3,8 @@ import React from 'react'
 function Footer() {
   return (
     <div class="container footer text-center">
-  <p>&copy; 2022 Copyright All Rights Reserved!</p>
-</div>
+      <p>&copy; 2022 Copyright All Rights Reserved!</p>
+    </div>
   )
 }
 
